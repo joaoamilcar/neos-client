2b03cacc004686dfac438b81a22a230a10e13fb2
João Amílcar <joaoamilcar@gmail.com>
2017-08-25 00:50:09 -0300

    first commit

---
+++ src/main/java/org/neos/server/Servidor.java
@@ -0,0 +1,28 @@
+package org.neos.server;
+
+public class Servidor {
+	private String host, porta;
+	
+	public Servidor(String host, String porta) {
+		setHost(host);
+		setPorta(porta);
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public String getPorta() {
+		return porta;
+	}
+
+	public void setPorta(String porta) {
+		this.porta = porta;
+	}
+	
+	
+}
